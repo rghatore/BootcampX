27d55898aa35fe803f5e7cd098be31cad6c42100
Rajdeep Ghatore <r.ghatore@gmail.com>
2020-10-13 05:17:10 +0000

    update database - two new tables

--- migrations/students_cohorts.sql
+++ migrations/students_cohorts.sql
@@ -5,10 +5,10 @@ CREATE TABLE cohorts (
   name VARCHAR(255) NOT NULL,
   start_date DATE,
   end_date DATE
-  );
+);
 
 
-  CREATE TABLE students (
+CREATE TABLE students (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
   email VARCHAR(255),
@@ -17,4 +17,4 @@ CREATE TABLE cohorts (
   start_date DATE,
   end_date DATE,
   cohort_id INTEGER REFERENCES cohorts(id) ON DELETE CASCADE
-  );
+);
