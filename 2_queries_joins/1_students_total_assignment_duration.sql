e0cba927e3e3e277986ea5691350776101eeaf17
Rajdeep Ghatore <r.ghatore@gmail.com>
2020-10-13 06:41:19 +0000

    join queries

---
+++ 2_queries_joins/1_students_total_assignment_duration.sql
@@ -0,0 +1,6 @@
+-- total amount of time that a student has spent on all assignments
+
+SELECT sum(assignment_submissions.duration) AS total_duration
+FROM assignment_submissions
+JOIN students ON student_id = students.id
+WHERE students.name = 'Ibrahim Schimmel';
---
+++ 2_queries_joins/2_cohorts_total_assignment_duration.sql
@@ -0,0 +1,7 @@
+-- total amount of time all students from a specific cohort have spent on all assignments
+
+SELECT sum(assignment_submissions.duration) AS total_duration
+FROM assignment_submissions
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = 'FEB12';
